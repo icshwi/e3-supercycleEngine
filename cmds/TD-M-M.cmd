3887cd4d584970f92e33a39bc41b7c0a98c4778d
Jerzy Jamroz <jerzy.jamroz@esss.se>
2020-03-06 15:06:20 +0100

    mrfioc2 integration of sce

--- cmds/TD-M-M.cmd
+++ cmds/TD-M-M.cmd
@@ -1,11 +1,11 @@
-#!../bin/linux-x86_64/supercycleEngine
-
-< envPaths
-cd "${TOP}"
-
 # Init environment
-iocshLoad("iocsh/mtcai.iocsh")
-iocshLoad("iocsh/tsi.iocsh")
+epicsEnvSet("TOP", "$(E3_CMD_TOP)/..")
+iocshLoad("$(TOP)/iocsh/mtcai.iocsh")
+iocshLoad("$(TOP)/iocsh/tsi.iocsh")
+
+# Maintenance
+epicsEnvSet("LOCATION", "MBL-070ROW:CNPW-U-017")
+epicsEnvSet("FBS",      "=ESS.INFR.K01.K01")
 
 epicsEnvSet("PSCE",     "TD-M:TS-SCE-01:")
 epicsEnvSet("PEVG",     "TD-M:TS-EVG-01:")
@@ -13,15 +13,21 @@ epicsEnvSet("PEVG",     "TD-M:TS-EVG-01:")
 epicsEnvUnset("PEVG")
 epicsEnvSet("PEVG",     "MTCA5U-EVG:")
 
-## Register all support components
-dbLoadDatabase "dbd/supercycleEngine.dbd"
-supercycleEngine_registerRecordDeviceDriver pdbbase
+epicsEnvSet("DBUFL",    "$(TOP)/../reftabs/init/databuffer-ess.json")
+epicsEnvSet("MEVTSL",   "$(TOP)/../reftabs/init/mevts-ess.json")
+epicsEnvSet("SCTROOT",  "$(TOP)/../reftabs/supercycles/")
+
+require("mrfioc2","2.2.0-rc7")
+require("supercycleEngine","master")
 
 ## Load record instances
 
-#dbLoadRecords("db/devg.db",   "P=$(PEVG)")
-iocshLoad("iocsh/scei.iocsh", "P=$(PSCE), PG=$(PEVG)")
+#dbLoadRecords("$(TOP)/db/devg.db",   "P=$(PEVG)")
+iocshLoad("$(TOP)/iocsh/evmi.iocsh", "P=MTCA5U, DEV=EVG, PCIID=$(MTCA_5U_PCIID7)")
+iocshLoad("$(TOP)/iocsh/scei.iocsh", "P=$(PSCE), PG=$(PEVG)")
 
-iocInit
+iocInit()
 
-#iocshLoad("iocsh/evgr.iocsh", "P=$(PEVG), INTREF=")
+iocshLoad("$(TOP)/iocsh/evgr.iocsh", "P=$(PEVG), INTREF=")
+
+dbl > "TD-M-M.pvlist"
