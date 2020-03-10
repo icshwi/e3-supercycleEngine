92d4cf3dbb1810e5a9a7e4899f71c01f1fabfd5f
Jerzy Jamroz <jerzy.jamroz@esss.se>
2020-03-10 11:10:00 +0100

    New cycle mask devel.

--- cmds/TD-M-M.cmd
+++ cmds/TD-M-M.cmd
@@ -1,33 +1,31 @@
 # Init environment
-epicsEnvSet("TOP", "$(E3_CMD_TOP)/..")
-iocshLoad("$(TOP)/iocsh/mtcai.iocsh")
-iocshLoad("$(TOP)/iocsh/tsi.iocsh")
+epicsEnvSet "TOP" "$(E3_CMD_TOP)/.."
+iocshLoad "$(TOP)/iocsh/mtcai.iocsh"
+iocshLoad "$(TOP)/iocsh/tsi.iocsh"
 
 # Maintenance
-epicsEnvSet("LOCATION", "MBL-070ROW:CNPW-U-017")
-epicsEnvSet("FBS",      "=ESS.INFR.K01.K01")
+epicsEnvSet "LOCATION" "MBL-070ROW:CNPW-U-017"
+epicsEnvSet "FBS"      "=ESS.INFR.K01.K01"
 
-epicsEnvSet("PSCE",     "TD-M:TS-SCE-01:")
-epicsEnvSet("PEVG",     "TD-M:TS-EVG-01:")
+epicsEnvSet "PSCE"     "TD-M:TS-SCE-01:"
+epicsEnvSet "PEVG"     "TD-M:TS-EVG-01:"
 
-epicsEnvUnset("PEVG")
-epicsEnvSet("PEVG",     "MTCA5U-EVG:")
+epicsEnvUnset "PEVG"
+epicsEnvSet "PEVG"     "MTCA5U-EVG:"
 
-epicsEnvSet("DBUFL",    "$(TOP)/../reftabs/init/databuffer-ess.json")
-epicsEnvSet("MEVTSL",   "$(TOP)/../reftabs/init/mevts-ess.json")
-epicsEnvSet("SCTROOT",  "$(TOP)/../reftabs/supercycles/")
+epicsEnvSet "DBUFL"    "$(TOP)/../reftabs/init/databuffer-ess.json"
+epicsEnvSet "MEVTSL"   "$(TOP)/../reftabs/init/mevts-ess.json"
+epicsEnvSet "SCTROOT"  "$(TOP)/../reftabs/supercycles/"
 
 require "mrfioc2" "2.2.0-rc7"
-require "supercycleEngine" "master"
+require "supercycleEngine" "develop"
 
 ## Load record instances
+iocshLoad "$(TOP)/iocsh/evmi.iocsh"  "P=MTCA5U,  DEV=EVG,   PCIID=$(MTCA_5U_PCIID7)"
+iocshLoad "$(TOP)/iocsh/scei.iocsh"  "P=$(PSCE), PG=$(PEVG)"
 
-#dbLoadRecords("$(TOP)/db/devg.db",   "P=$(PEVG)")
-iocshLoad("$(TOP)/iocsh/evmi.iocsh", "P=MTCA5U, DEV=EVG, PCIID=$(MTCA_5U_PCIID7)")
-iocshLoad("$(TOP)/iocsh/scei.iocsh", "P=$(PSCE), PG=$(PEVG)")
+iocInit
 
-iocInit()
-
-iocshLoad("$(TOP)/iocsh/evgr.iocsh", "P=$(PEVG), INTREF=")
+iocshLoad "$(TOP)/iocsh/evgr.iocsh" "P=$(PEVG), INTREF="
 
 dbl > "TD-M-M.pvlist"
