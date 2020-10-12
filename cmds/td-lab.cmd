b2381d06fde5e33a7172724e6914f8a3a371b340
Jerzy Jamroz <jerzy.jamroz@esss.se>
2020-10-12 17:41:32 +0200

    new feature devel

---
+++ cmds/td-lab.cmd
@@ -0,0 +1,29 @@
+# .cmd
+#require "iocstats" 
+require "mrfioc2" "2.2.1rc1"
+require "supercycleEngine" "master"
+
+epicsEnvSet "TOP" "$(E3_CMD_TOP)/.."
+
+epicsEnvSet "ENGINEER" "ICS_HWI_WP04"
+epicsEnvSet "EPICS_CA_MAX_ARRAY_BYTES" "10000000"
+
+epicsEnvSet "PSCE"          "TD-L5U1:Ctrl-SCE-1"
+epicsEnvSet "PEVG"          "TD-L5U1:Ctrl-EVG-1"
+epicsEnvSet "reftabs_DIR"   "/opt/reftabs"
+
+epicsEnvSet "DBUFL"         "$(reftabs_DIR)/init/databuffer-ess.json"
+epicsEnvSet "MEVTSL"        "$(reftabs_DIR)/init/mevts-ess.json"
+epicsEnvSet "SCTROOT"       "$(reftabs_DIR)/supercycles/"
+
+# Load record instances
+iocshLoad "$(mrfioc2_DIR)/evm.iocsh"            "P=$(PEVG), OBJ=EVG,   PCIID=$(PCI_SLOT=08:00.0)"
+iocshLoad "$(TOP)/iocsh/sce.iocsh"              "P=$(PSCE), PG=$(PEVG)"
+
+iocInit
+
+iocshLoad "$(mrfioc2_DIR)/evgr.iocsh"           "P=$(PEVG), INTRF=$(INTRF=), INTPPS=$(INTPPS=)"
+iocshLoad "$(mrfioc2_DIR)/evgasynr.iocsh"       "P=$(PEVG), $(ASYNARGS=)"
+iocshLoad "$(mrfioc2_DIR)/evgseqr.iocsh"        "P=$(PEVG)"
+
+# EOF
