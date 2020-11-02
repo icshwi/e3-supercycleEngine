2cccec0ff3179ea6dca2f62be37de82104714032
Jerzy Jamroz <jerzy.jamroz@esss.se>
2020-11-02 17:34:46 +0100

    better scroot handling

--- cmds/td-lab.cmd
+++ cmds/td-lab.cmd
@@ -1,4 +1,4 @@
-# .cmd
+#!iocsh.bash
 #require "iocstats"
 require "mrfioc2" "master"
 require "supercycleEngine" "devel"
@@ -7,14 +7,10 @@ epicsEnvSet "TOP" "$(E3_CMD_TOP)/.."
 
 epicsEnvSet "ENGINEER" "ICS_HWI_WP04"
 epicsEnvSet "EPICS_CA_MAX_ARRAY_BYTES" "10000000"
+var "iodebug" "3"
 
 epicsEnvSet "PSCE"          "TD-L5U1:Ctrl-SCE-1"
 epicsEnvSet "PEVG"          "TD-L5U1:Ctrl-EVG-1"
-epicsEnvSet "reftabs_DIR"   "/opt/reftabs"
-
-epicsEnvSet "DBUFL"         "$(reftabs_DIR)/init/databuffer-ess.yml"
-epicsEnvSet "MEVTSL"        "$(reftabs_DIR)/init/mevts-ess.yml"
-epicsEnvSet "SCTROOT"       "$(reftabs_DIR)/supercycles/"
 
 # Load record instances
 iocshLoad "$(mrfioc2_DIR)/evm.iocsh"            "P=$(PEVG), OBJ=EVG,   PCIID=$(PCI_SLOT=08:00.0)"
