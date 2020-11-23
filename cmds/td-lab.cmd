af8ab403ade14aafb0f7ff08cffef27beaedfea3
Jerzy Jamroz <jerzy.jamroz@gmail.com>
2020-11-23 16:26:32 +0100

    devel: sim added

--- cmds/td-lab.cmd
+++ cmds/td-lab.cmd
@@ -1,4 +1,4 @@
-#!iocsh.bash
+#!/usr/bin/env iocsh.bash
 #require "iocstats"
 require "mrfioc2" "master"
 require "supercycleEngine" "devel"
@@ -6,20 +6,17 @@ require "supercycleEngine" "devel"
 epicsEnvSet "TOP" "$(E3_CMD_TOP)/.."
 
 epicsEnvSet "ENGINEER" "ICS_HWI_WP04"
-epicsEnvSet "EPICS_CA_MAX_ARRAY_BYTES" "10000000"
 
-epicsEnvSet "PSCE"          "TD-L5U1:Ctrl-SCE-1"
-epicsEnvSet "PEVG"          "TD-L5U1:Ctrl-EVG-1"
+epicsEnvSet "PSCE" "TD-L5U1:Ctrl-SCE-1"
+epicsEnvSet "PEVG" "TD-L5U1:Ctrl-EVG-1"
 
 # Load record instances
-iocshLoad "$(mrfioc2_DIR)/evm.iocsh"            "P=$(PEVG), OBJ=EVG,   PCIID=$(PCI_SLOT=08:00.0)"
-iocshLoad "$(TOP)/iocsh/sce.iocsh"              "P=$(PSCE), PG=$(PEVG)"
+iocshLoad "$(mrfioc2_DIR)/evm.iocsh"            "P=$(PEVG),OBJ=EVG,PCIID=$(PCI_SLOT=08:00.0)"
+iocshLoad "$(TOP)/iocsh/sce.iocsh"              "P=$(PSCE),PG=$(PEVG)"
 
 #var "iodebug" "3"
 iocInit
 
-iocshLoad "$(mrfioc2_DIR)/evgr.iocsh"           "P=$(PEVG), INTRF=$(INTRF=), INTPPS=$(INTPPS=)"
-iocshLoad "$(mrfioc2_DIR)/evgasynr.iocsh"       "P=$(PEVG), $(ASYNARGS=)"
+iocshLoad "$(mrfioc2_DIR)/evgr.iocsh"           "P=$(PEVG),INTRF=$(INTRF=),INTPPS=$(INTPPS=)"
+iocshLoad "$(mrfioc2_DIR)/evgasynr.iocsh"       "P=$(PEVG),$(ASYNARGS=)"
 iocshLoad "$(mrfioc2_DIR)/evgseqr.iocsh"        "P=$(PEVG)"
-
-# EOF
---
+++ cmds/td-lab_sim.cmd
@@ -0,0 +1,15 @@
+#!/usr/bin/env iocsh.bash
+require "supercycleEngine" "devel"
+
+epicsEnvSet "TOP" "$(E3_CMD_TOP)/.."
+
+epicsEnvSet "ENGINEER" "ICS_HWI_WP04"
+
+epicsEnvSet "PSCE" "TD-L5U1:Ctrl-SCE-1"
+epicsEnvSet "PEVG" "TD-L5U1:Ctrl-EVG-1"
+
+# Load record instances
+dbLoadRecords "db/devg.db" "P=$(PEVG)"
+iocshLoad     "$(supercycleEngine_DIR)/sce.iocsh"   "P=$(PSCE), PG=$(PEVG)"
+#var "iodebug" "3"
+iocInit
